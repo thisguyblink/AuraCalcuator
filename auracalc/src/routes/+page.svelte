8fb36b51e771cc61feb32bf60c728b6eec0b9cd9
thisguyblink <patrickliamsmith@gmail.com>
2024-06-15 02:37:52 -0400

    Made Sveltekit files and worked on homepage

---
+++ auracalc/src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+
+</script>
+
+<div class = header>
+    <h1>Aura Calcualator</h1>
+    <h3>Take it if you dare!</h3>
+    <h3>Are you a Alpha or a Sigma...</h3>
+</div>
+<div class =questionList>
+    <div class = question>
+        <label class="custom-checkbox">
+            <input type="checkbox">
+            1. Have you rizzed up at least 10 people in the last week?
+        </label>
+    </div>
+</div>
+    
+<style>
+    .header {
+        padding-top: 3rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+    .questionList {
+        padding-top: 3rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+</style>
+
