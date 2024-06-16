d8e0b0ad98ebd8c636202a4e8970882f782d5535
thisguyblink <patrickliamsmith@gmail.com>
2024-06-16 13:35:07 -0400

    Some functions work, I need stil need to make checkbox dynamic

--- auracalc/src/routes/+page.svelte
+++ auracalc/src/routes/+page.svelte
@@ -1,60 +1,36 @@
 <script>
-    // document.getElementById('submitButton').addEventListener('click', function() {
-    //     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    //   let totalValue = 0;
-    //   checkboxes.forEach(checkbox => {
-    //     if (checkbox.checked) {
-    //       totalValue += parseInt(checkbox.value);
-    //     }
-    //   });
-    // });
+    const values = [74, -101, -523, 711, 780, -163, 127, -47, -588, -600, 87, -519, -211, 150, -512, 407, 237, -125, 870, 986, 93, 198, 505, -985, -444, 395, -121, 913, 83, -990, 255, -54, -145, 793, 448, -964, 264, -167, 509, 209, 981, 775, -813, 96, 838, -622, -217, 565, -394, 132];
+    let out = 'Cowabunga';
+    function handleSubmit() {
+        out = values[0];
+        
+    }
+    function reset() {
+        if (window.confirm('Really opt-in?')) {
+            out = 'score has been reset(0)';
+        }
+    }
 </script>
 
 <div class = header>
     <h1>Aura Calcualator</h1>
     <h3>Take it if you dare!</h3>
-    <h3>Are you a Alpha or a Sigma...</h3>
+    <h3>Are you a SIGMA or beta...</h3>
 </div>
 <div class ="questionList">
-    <div class = question>
-        <label class="custom-checkbox">
-            <input type="checkbox" value="100">
-            1. Have you rizzed up at least 10 people in the last week?
-        </label>
-    </div>
-    <div class = question>
-        <label class="custom-checkbox">
-            <input type="checkbox" value="15">
-            2. Finished something earlier than expected?
-        </label>
-    </div>
-    <div class = question>
-        <label class="custom-checkbox">
-            <input type="checkbox" value="-60">
-            3. Was late at least once?
-        </label>
-    </div>
-    <div class = question>
-        <label class="custom-checkbox">
-            <input type="checkbox" value="-450">
-            4. Got a flat tire?
-        </label>
-    </div>
-    <div class = question>
-        <label class="custom-checkbox">
-            <input type="checkbox" value="-100">
-            5. Dropped something?
-        </label>
-    </div>
-    <div class = question>
-        <label class="custom-checkbox">
-            <input type="checkbox" value="4500">
-            6. Almost died but didn't?
-        </label>
-    </div>
-    
-    <span></span>
-    <button id ="submitButton">Submit</button>
+    <form class="questionForm">
+        <input type="checkbox" id="q1" >
+        <label for="q1">1. Rizzed up at least 1 person in the last week</label>
+        <br>
+        <button type="button" id="submit" on:click={() => {
+           handleSubmit();
+        }}>Submit</button>
+        <button type="button" id="reset" on:click={() => {
+            reset();
+         }}>Reset</button>
+         
+    </form>
+    <p id="output">Your score is {out}</p>
 </div>
     
 <style>
@@ -72,8 +48,15 @@
         align-items: center;
         flex-direction: column;
     }
-    .checkbox:hover {
-        background-color: black;
+    #submit {
+        background-color: green;
+        color: blue
+    }
+    #output {
+        border: 3px solid purple;
+        height: 15rem;
+        width: 15rem;
+        background-color: beige;
     }
 
 </style>
