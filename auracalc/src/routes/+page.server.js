971923d9c4a6dd7af36d7e4129f84aadcfda41ed
thisguyblink <patrickliamsmith@gmail.com>
2024-06-16 17:21:47 -0400

    more features but score still doesn't work :(

---
+++ auracalc/src/routes/+page.server.js
@@ -0,0 +1,10 @@
+// /** @type {import('./$types').Actions} */
+// export const actions = {
+// 	submission: async ({request }) => {
+// 		const data = await request.formData();
+//         console.log(data);
+// 		const email = data.get('fname');
+// 		const password = data.get('lname');
+// 		return { success: true };
+// 	}
+// }
--- auracalc/src/routes/+page.svelte
+++ auracalc/src/routes/+page.svelte
@@ -1,15 +1,35 @@
 <script>
     const values = [74, -101, -523, 711, 780, -163, 127, -47, -588, -600, 87, -519, -211, 150, -512, 407, 237, -125, 870, 986, 93, 198, 505, -985, -444, 395, -121, 913, 83, -990, 255, -54, -145, 793, 448, -964, 264, -167, 509, 209, 981, 775, -813, 96, 838, -622, -217, 565, -394, 132];
     let out = 'Cowabunga';
+    let total = 0;
+    let dialog;
+    let val;
+    let check = false;
+    import alpha from '$lib/assets/alpha wolf.jpeg';
+
     function handleSubmit() {
         out = values[0];
-        
     }
     function reset() {
-        if (window.confirm('Really opt-in?')) {
+        if (window.confirm('Are you sure you want to reset your input?')) {
             out = 'score has been reset(0)';
         }
     }
+    async function getForm()  {
+        let form = await request.formData()
+        let group = form.getAll('value')
+        window.confirm(group);
+    }
+    function showPop() {
+        dialog.showModal();
+    }
+    function closePop() {
+        dialog.close();
+    }
+    function share() {
+        window.confirm("Results have been copied to clipboard");
+    }
+    
 </script>
 
 <div class = header>
@@ -18,19 +38,44 @@
     <h3>Are you a SIGMA or beta...</h3>
 </div>
 <div class ="questionList">
-    <form class="questionForm">
-        <input type="checkbox" id="q1" >
-        <label for="q1">1. Rizzed up at least 1 person in the last week</label>
+    <form class="questionForm" method="POST" action="?/submission">
+        <input type="checkbox" id="q1" bind:checked={check}>
+        <label for="q1">1. Rizzed up at least 1 person in the last week: {values[4]}</label>
+        <br>
+        <input type="checkbox" id="q2" value="1">
+        <label for="q2">1. Rizzed up at least 1 person in the last week: {values[4]}</label>
+        <br>
+        <input type="checkbox" id="q3" value="2">
+        <label for="q3">1. Rizzed up at least 1 person in the last week: {values[4]}</label>
+        <br>
+        <input type="checkbox" id="q4" value="3">
+        <label for="q4">1. Rizzed up at least 1 person in the last week: {values[4]}</label>
+        
         <br>
         <button type="button" id="submit" on:click={() => {
            handleSubmit();
+           getForm();
         }}>Submit</button>
         <button type="button" id="reset" on:click={() => {
             reset();
          }}>Reset</button>
-         
     </form>
-    <p id="output">Your score is {out}</p>
+    <p id="output">Your score is {total}</p>
+    <div class = "results">
+        <dialog id="dresult" bind:this={dialog}>
+            <img alt="No found" src={alpha} />
+            <p>YOUR AURA IS {total}</p>
+            <button type="button" on:click={() => {
+                closePop();
+            }}>Exit</button>
+             <button type="button" on:click={() => {
+                 share();
+                }}>Share</button>
+        </dialog>
+    </div>
+    <button type="button" on:click={() => {
+        showPop();
+        }}>Results</button>
 </div>
     
 <style>
